a63b3dbd69decbc8acbb262574f4e764411ff9a4
MichaelSukkiSong <sa77irak@gmail.com>
2023-12-23 20:36:55 -0800

    added cron jobs, deleted fetchjobs func

--- src/app/(main)/components/panel/main/JDgrid.js
+++ src/app/(main)/components/panel/main/JDgrid.js
@@ -1,55 +1,55 @@
+'use client';
+
 import JDcard from './JDcard';
-import fetchJobs from '@/app/utils/fetchJobs';
-import { collection, addDoc, getDocs, where, query } from 'firebase/firestore';
-import fb from '@/app/services/firebase';
+// import { collection, addDoc, getDocs, where, query } from 'firebase/firestore';
+// import fb from '@/app/services/firebase';
 
-const JDgrid = async () => {
-  const { jobs, meta } = await fetchJobs();
+const JDgrid = () => {
+  // const { jobs, meta } = await fetchJobs();
+  // const db = fb.getFirestore();
+  // for (const job of jobs) {
+  //   // Check if the job already exists in Firestore
+  //   const jobQuery = query(
+  //     collection(db, 'jobs'),
+  //     where('job_title', '==', job.title),
+  //     where('job_location', '==', job.location.name),
+  //     where(
+  //       'job_entity',
+  //       '==',
+  //       job.metadata.find((el) => el.name === 'Entity')?.value || null
+  //     ),
+  //     where(
+  //       'job_type',
+  //       '==',
+  //       job.metadata.find((el) => el.name === 'Time Type')?.value || null
+  //     )
+  //   );
+  //   const querySnapshot = await getDocs(jobQuery);
 
-  const db = fb.getFirestore();
+  //   if (querySnapshot.empty) {
+  //     // Job does not exist, add it to Firestore
+  //     try {
+  //       const docRef = await addDoc(collection(db, 'jobs'), {
+  //         job_title: job.title,
+  //         job_location: job.location.name,
+  //         job_entity: job.metadata.find((el) => el.name === 'Entity').value,
+  //         job_type: job.metadata.find((el) => el.name === 'Time Type').value,
+  //         compensation: null,
+  //         salary_range: null,
+  //       });
 
-  for (const job of jobs) {
-    // Check if the job already exists in Firestore
-    const jobQuery = query(
-      collection(db, 'jobs'),
-      where('job_title', '==', job.title),
-      where('job_location', '==', job.location.name),
-      where(
-        'job_entity',
-        '==',
-        job.metadata.find((el) => el.name === 'Entity')?.value || null
-      ),
-      where(
-        'job_type',
-        '==',
-        job.metadata.find((el) => el.name === 'Time Type')?.value || null
-      )
-    );
-    const querySnapshot = await getDocs(jobQuery);
+  //       console.log('Document written with ID: ', docRef.id);
+  //     } catch (e) {
+  //       console.error('Error adding document: ', e);
+  //     }
+  //   } else {
+  //     // Job already exists, skip
+  //     console.log(
+  //       `Job "${job.title}" already exists in Firestore. Skipping...`
+  //     );
+  //   }
+  // }
 
-    if (querySnapshot.empty) {
-      // Job does not exist, add it to Firestore
-      try {
-        const docRef = await addDoc(collection(db, 'jobs'), {
-          job_title: job.title,
-          job_location: job.location.name,
-          job_entity: job.metadata.find((el) => el.name === 'Entity').value,
-          job_type: job.metadata.find((el) => el.name === 'Time Type').value,
-          compensation: null,
-          salary_range: null,
-        });
-
-        console.log('Document written with ID: ', docRef.id);
-      } catch (e) {
-        console.error('Error adding document: ', e);
-      }
-    } else {
-      // Job already exists, skip
-      console.log(
-        `Job "${job.title}" already exists in Firestore. Skipping...`
-      );
-    }
-  }
   const renderJDcard = () => {
     return <JDcard />;
   };
