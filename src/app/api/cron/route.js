e4a46a2782ed6ee5144c82e7afe36349e37978e6
MichaelSukkiSong <sa77irak@gmail.com>
2023-12-25 10:34:01 -0800

    change maxDuration to 300s for all functions

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,4 +1,5 @@
-// export const runtime = 'edge';
+import fb from '@/app/services/firebase';
+import { collection, addDoc, getDocs, where, query } from 'firebase/firestore';
 
 export async function GET(request) {
   // Secure cron function invocation by checking header
@@ -9,13 +10,66 @@ export async function GET(request) {
     });
   }
 
-  const baseUrl = process.env.VERCEL_URL
-    ? 'https://' + process.env.VERCEL_URL
-    : 'http://localhost:3000';
-
-  await fetch(`${baseUrl}/api/jobs`, {
-    headers: request.headers,
-  });
+  const { jobs, meta } = await getJobs();
+  const db = fb.getFirestore();
+  await saveJobs(db, jobs);
 
   return Response.json({ success: true, message: 'Cron job completed' });
+}
+
+async function getJobs() {
+  const res = await fetch(
+    'https://boards-api.greenhouse.io/v1/boards/doordash/jobs'
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch jobs');
+  }
+
+  return res.json();
+}
+
+async function saveJobs(db, jobs) {
+  for (const job of jobs) {
+    // Check if the job already exists in Firestore
+    const jobQuery = query(
+      collection(db, 'jobs'),
+      where('job_title', '==', job.title),
+      where('job_location', '==', job.location.name),
+      where(
+        'job_entity',
+        '==',
+        job.metadata.find((el) => el.name === 'Entity')?.value || null
+      ),
+      where(
+        'job_type',
+        '==',
+        job.metadata.find((el) => el.name === 'Time Type')?.value || null
+      )
+    );
+    const querySnapshot = await getDocs(jobQuery);
+
+    if (querySnapshot.empty) {
+      // Job does not exist, add it to Firestore
+      try {
+        const docRef = await addDoc(collection(db, 'jobs'), {
+          job_title: job.title,
+          job_location: job.location.name,
+          job_entity: job.metadata.find((el) => el.name === 'Entity').value,
+          job_type: job.metadata.find((el) => el.name === 'Time Type').value,
+          compensation: null,
+          salary_range: null,
+        });
+
+        console.log('Document written with ID: ', docRef.id);
+      } catch (e) {
+        console.error('Error adding document: ', e);
+      }
+    } else {
+      // Job already exists, skip
+      console.log(
+        `Job "${job.title}" already exists in Firestore. Skipping...`
+      );
+    }
+  }
 }
