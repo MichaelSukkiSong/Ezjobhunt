80b71214dc98050b88ed86c6e1ceeac96a2d2b5b
MichaelSukkiSong <sa77irak@gmail.com>
2023-12-31 12:28:45 -0800

    added job title,location,company to job data

--- functions/index.js
+++ functions/index.js
@@ -13,12 +13,36 @@ exports.makejobdescriptionJSON = onDocumentCreated(
   "/greenhouse/{greenhouseId}",
   async (event) => {
     const job = event.data;
+    const jobetc_JSON = JSON.stringify({
+      job_title: job.data().title,
+      job_location: job.data().location.name,
+      job_company: job.data().metadata.find((el) => el.name === "Entity").value,
+    });
     const jobDescription_JSON = await processJob(job);
+    const job_obj = combineJson(jobetc_JSON, jobDescription_JSON);
 
-    if (jobDescription_JSON) {
+    if (job_obj) {
       const db = getFirestore();
 
-      await db.collection("jobs").add(JSON.parse(jobDescription_JSON));
+      await db.collection("jobs").add(job_obj);
     }
   }
 );
+
+function combineJson(jsonString1, jsonString2) {
+  try {
+    // Parse the JSON strings into objects
+    const object1 = JSON.parse(jsonString1);
+    const object2 = JSON.parse(jsonString2);
+
+    // Combine the objects
+    const combinedObject = { ...object1, ...object2 };
+
+    // Return the combined object
+    return combinedObject;
+  } catch (error) {
+    // Handle any errors that might occur during the parsing or combining process
+    console.error("Error combining JSON:", error);
+    return null;
+  }
+}
