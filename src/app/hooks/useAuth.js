690df745270cda2034c9663623e21a451f7c7d40
MichaelSukkiSong <sa77irak@gmail.com>
2024-01-04 17:22:46 -0800

    add user to firestore inside useAuth hook

--- src/app/hooks/useAuth.js
+++ src/app/hooks/useAuth.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
+import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
 import fb from "../services/firebase";
 
 export function useAuth() {
@@ -7,8 +8,44 @@ export function useAuth() {
 
   useEffect(() => {
     const auth = fb.getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      console.log("user : ", user);
+
+      if (user) {
+        const db = fb.getFirestore();
+
+        // Check if the user already exists in Firestore
+        const querySnapshot = await getDocs(
+          query(collection(db, "users"), where("uid", "==", user.uid))
+        );
+
+        if (querySnapshot.empty) {
+          // user does not exist, add it to Firestore
+          try {
+            const docRef = await addDoc(collection(db, "users"), {
+              uid: user?.uid,
+              name: user?.displayName,
+              email: user?.email,
+              photoURL: user?.photoURL,
+              providerData: user?.providerData[0],
+              saved: [],
+              applied: [],
+              interviewing: [],
+              rejected: [],
+              hidden: [],
+            });
+
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+        } else {
+          // user already exists, skip
+          console.log(
+            `user "${user.uid}" already exists in Firestore. Skipping...`
+          );
+        }
+      }
 
       setUser(user);
     });
