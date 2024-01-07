faf6bcdc281896d5546baa243fab0f3be8192795
MichaelSukkiSong <sa77irak@gmail.com>
2024-01-06 16:25:57 -0800

    implement application-tracker saved section

--- src/app/(main)/application-tracker/page.js
+++ src/app/(main)/application-tracker/page.js
@@ -1,10 +1,93 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { Icon } from "@chakra-ui/react";
 import { MdSettings } from "../icons";
+import { useAuth } from "@/app/hooks/useAuth";
+import fb from "@/app/services/firebase";
+import JDcard from "../components/panel/main/JDcard";
 
 const Page = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [currentUserUid, setCurrentUserUid] = useState(null);
+  const [savedJobsId, setSavedJobsId] = useState([]);
+  const [savedJobs, setSavedJobs] = useState([]);
+  const user = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      setCurrentUserUid(user?.uid);
+      // console.log(user.uid);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const getSavedJobsId = async () => {
+      if (!currentUserUid) return;
+
+      const db = fb.getFirestore();
+
+      // Query the user by uid
+      const q = query(
+        collection(db, "users"),
+        where("uid", "==", currentUserUid)
+      );
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.docs.length > 0) {
+        const userDoc = querySnapshot.docs[0].data();
+
+        // get saved jobsId and set it to state
+        setSavedJobsId(userDoc.saved || []);
+      }
+    };
+
+    getSavedJobsId();
+  }, [currentUserUid]);
+
+  useEffect(() => {
+    // get saved jobs object
+    const getSavedJobs = async () => {
+      if (!savedJobsId) return;
+
+      const db = fb.getFirestore();
+
+      const querySnapshot = await getDocs(collection(db, "jobs"));
+      const savedJobsArray = [];
+      querySnapshot.forEach((doc) => {
+        if (savedJobsId.includes(doc.id)) {
+          savedJobsArray.push({ id: doc.id, ...doc.data() });
+        }
+      });
+
+      setSavedJobs(savedJobsArray);
+    };
+
+    getSavedJobs();
+  }, [savedJobsId]);
+
+  const handleSaveJobClick = () => {};
+  const handleMarkAppliedClick = () => {};
+  const handleHideJobClick = () => {};
+  const handleReportJobClick = () => {};
+
+  const renderJDcard = () => {
+    return savedJobs.map((job) => {
+      return (
+        <JDcard
+          key={job.id}
+          job={job}
+          handleSaveJobClick={handleSaveJobClick}
+          handleMarkAppliedClick={handleMarkAppliedClick}
+          handleHideJobClick={handleHideJobClick}
+          handleReportJobClick={handleReportJobClick}
+        />
+      );
+    });
+  };
+
   return (
     <div className="md:h-screen overflow-x-hidden w-full">
       <div className="flex flex-col">
@@ -38,7 +121,9 @@ const Page = () => {
               </button>
             </div>
             <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 md:items-center md:space-x-4 mt-2 p-4">
-              <span className="text-2xl font-bold">Your saved jobs (0)</span>
+              <span className="text-2xl font-bold">
+                Your saved jobs ({savedJobs.length})
+              </span>
               <div>
                 <button className="underline font-medium">
                   Add an external job
@@ -46,16 +131,25 @@ const Page = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col flex-auto items-center p-8 my-8 border rounded-xl m-4">
-            <span className="text-xl font-bold">No saved jobs</span>
-            <span className="mt-2 font-light">
-              Start &nbsp;
-              <Link className="text-blue-600" href="/">
-                adding jobs
-              </Link>
-              &nbsp; to your list.
-            </span>
+          <div className="infinite-scroll-component__outerdiv">
+            <div className="infinite-scroll-component ">
+              <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+                {renderJDcard()}
+              </div>
+            </div>
           </div>
+          {savedJobs ? null : (
+            <div className="flex flex-col flex-auto items-center p-8 my-8 border rounded-xl m-4">
+              <span className="text-xl font-bold">No saved jobs</span>
+              <span className="mt-2 font-light">
+                Start &nbsp;
+                <Link className="text-blue-600" href="/">
+                  adding jobs
+                </Link>
+                &nbsp; to your list.
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
