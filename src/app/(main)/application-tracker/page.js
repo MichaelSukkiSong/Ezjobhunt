78866400438f4c905cefcc5cb5f94e16a5d6a42d
MichaelSukkiSong <sa77irak@gmail.com>
2024-01-08 17:38:05 -0800

    implemented applied/hidden job section of application-tracker

--- src/app/(main)/application-tracker/page.js
+++ src/app/(main)/application-tracker/page.js
@@ -13,23 +13,27 @@ const Page = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [currentUserUid, setCurrentUserUid] = useState(null);
   const [savedJobsId, setSavedJobsId] = useState([]);
+  const [appliedJobsId, setAppliedJobsId] = useState([]);
+  const [hiddenJobsId, setHiddenJobsId] = useState([]);
   const [savedJobs, setSavedJobs] = useState([]);
+  const [appliedJobs, setAppliedJobs] = useState([]);
+  const [interviewingJobs, setInterviewingJobs] = useState([]);
+  const [rejectedJobs, setRejectedJobs] = useState([]);
+  const [hiddenJobs, setHiddenJobs] = useState([]);
   const user = useAuth();
 
   useEffect(() => {
     if (user) {
       setCurrentUserUid(user?.uid);
-      // console.log(user.uid);
     }
   }, [user]);
 
   useEffect(() => {
-    const getSavedJobsId = async () => {
+    const getSavedAppliedHiddenJobsId = async () => {
       if (!currentUserUid) return;
 
       const db = fb.getFirestore();
 
-      // Query the user by uid
       const q = query(
         collection(db, "users"),
         where("uid", "==", currentUserUid)
@@ -39,16 +43,16 @@ const Page = () => {
       if (querySnapshot.docs.length > 0) {
         const userDoc = querySnapshot.docs[0].data();
 
-        // get saved jobsId and set it to state
         setSavedJobsId(userDoc.saved || []);
+        setAppliedJobsId(userDoc.applied || []);
+        setHiddenJobsId(userDoc.hidden || []);
       }
     };
 
-    getSavedJobsId();
+    getSavedAppliedHiddenJobsId();
   }, [currentUserUid]);
 
   useEffect(() => {
-    // get saved jobs object
     const getSavedJobs = async () => {
       if (!savedJobsId) return;
 
@@ -68,24 +72,173 @@ const Page = () => {
     getSavedJobs();
   }, [savedJobsId]);
 
+  useEffect(() => {
+    const getAppliedJobs = async () => {
+      if (!appliedJobsId) return;
+
+      const db = fb.getFirestore();
+
+      const querySnapshot = await getDocs(collection(db, "jobs"));
+      const appliedJobsArray = [];
+      querySnapshot.forEach((doc) => {
+        if (appliedJobsId.includes(doc.id)) {
+          appliedJobsArray.push({ id: doc.id, ...doc.data() });
+        }
+      });
+
+      setAppliedJobs(appliedJobsArray);
+    };
+
+    getAppliedJobs();
+  }, [appliedJobsId]);
+
+  useEffect(() => {
+    const getHiddenJobs = async () => {
+      if (!hiddenJobsId) return;
+
+      const db = fb.getFirestore();
+
+      const querySnapshot = await getDocs(collection(db, "jobs"));
+      const hiddenJobsArray = [];
+      querySnapshot.forEach((doc) => {
+        if (hiddenJobsId.includes(doc.id)) {
+          hiddenJobsArray.push({ id: doc.id, ...doc.data() });
+        }
+      });
+
+      setHiddenJobs(hiddenJobsArray);
+    };
+
+    getHiddenJobs();
+  }, [hiddenJobsId]);
+
   const handleSaveJobClick = () => {};
   const handleMarkAppliedClick = () => {};
   const handleHideJobClick = () => {};
   const handleReportJobClick = () => {};
 
-  const renderJDcard = () => {
-    return savedJobs.map((job) => {
-      return (
-        <JDcard
-          key={job.id}
-          job={job}
-          handleSaveJobClick={handleSaveJobClick}
-          handleMarkAppliedClick={handleMarkAppliedClick}
-          handleHideJobClick={handleHideJobClick}
-          handleReportJobClick={handleReportJobClick}
-        />
-      );
-    });
+  const renderContent = () => {
+    switch (activeIndex) {
+      case 0:
+        return renderSavedJobs();
+      case 1:
+        return renderAppliedJobs();
+      case 2:
+        return renderInterviewingJobs();
+      case 3:
+        return renderRejectedJobs();
+      case 4:
+        return renderHiddenJobs();
+      default:
+        return null;
+    }
+  };
+
+  const renderSavedJobs = () => {
+    return (
+      <>
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+          {savedJobs.map((job) => (
+            <JDcard
+              key={job.id}
+              job={job}
+              handleSaveJobClick={handleSaveJobClick}
+              handleMarkAppliedClick={handleMarkAppliedClick}
+              handleHideJobClick={handleHideJobClick}
+              handleReportJobClick={handleReportJobClick}
+            />
+          ))}
+        </div>
+        {savedJobs.length === 0 && (
+          <div className="flex flex-col flex-auto items-center p-8 my-8 border rounded-xl m-4">
+            <span className="text-xl font-bold">No saved jobs</span>
+            <span className="mt-2 font-light">
+              Start &nbsp;
+              <Link className="text-blue-600" href="/">
+                adding jobs
+              </Link>
+              &nbsp; to your list.
+            </span>
+          </div>
+        )}
+      </>
+    );
+  };
+
+  const renderAppliedJobs = () => {
+    // Implement rendering for Applied Jobs
+    // ...
+    return (
+      <>
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+          {appliedJobs.map((job) => (
+            <JDcard
+              key={job.id}
+              job={job}
+              handleSaveJobClick={handleSaveJobClick}
+              handleMarkAppliedClick={handleMarkAppliedClick}
+              handleHideJobClick={handleHideJobClick}
+              handleReportJobClick={handleReportJobClick}
+            />
+          ))}
+        </div>
+        {appliedJobs.length === 0 && (
+          <div className="flex flex-col flex-auto items-center p-8 my-8 border rounded-xl m-4">
+            <span className="text-xl font-bold">No saved jobs</span>
+            <span className="mt-2 font-light">
+              Start &nbsp;
+              <Link className="text-blue-600" href="/">
+                adding jobs
+              </Link>
+              &nbsp; to your list.
+            </span>
+          </div>
+        )}
+      </>
+    );
+  };
+
+  const renderInterviewingJobs = () => {
+    // Implement rendering for Interviewing Jobs
+    // ...
+  };
+
+  const renderRejectedJobs = () => {
+    // Implement rendering for Rejected Jobs
+    // ...
+  };
+
+  const renderHiddenJobs = () => {
+    // Implement rendering for Hidden Jobs
+    // ...
+    return (
+      <>
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+          {hiddenJobs.map((job) => (
+            <JDcard
+              key={job.id}
+              job={job}
+              handleSaveJobClick={handleSaveJobClick}
+              handleMarkAppliedClick={handleMarkAppliedClick}
+              handleHideJobClick={handleHideJobClick}
+              handleReportJobClick={handleReportJobClick}
+            />
+          ))}
+        </div>
+        {hiddenJobs.length === 0 && (
+          <div className="flex flex-col flex-auto items-center p-8 my-8 border rounded-xl m-4">
+            <span className="text-xl font-bold">No saved jobs</span>
+            <span className="mt-2 font-light">
+              Start &nbsp;
+              <Link className="text-blue-600" href="/">
+                adding jobs
+              </Link>
+              &nbsp; to your list.
+            </span>
+          </div>
+        )}
+      </>
+    );
   };
 
   return (
@@ -93,36 +246,69 @@ const Page = () => {
       <div className="flex flex-col">
         <div className="flex flex-col">
           <div className="flex flex-col">
-            {/* <div className="p-4 md:hidden">
-              <div className="w-fit">
-                <Link href="/">
-                  <div className="flex items-center space-x-1">
-                    <Icon as={MdSettings} className="h-6 w-6" />
-                    <span className="text-xl font-extrabold">Ezjobhunt</span>
-                  </div>
-                </Link>
-              </div>
-            </div> */}
             <div className="flex items-center space-x-2 lg:space-x-4 mx-4 pt-4 pb-2 overflow-x-auto scrollbar-hide bg-white">
-              <button className="border text-sm lg:text-base px-2 py-1 lg:px-4 rounded border-black text-black font-bold">
+              <button
+                onClick={() => setActiveIndex(0)}
+                className={`border text-sm lg:text-base px-2 py-1 lg:px-4 rounded  ${
+                  activeIndex === 0
+                    ? "border-black text-black font-bold"
+                    : "hover:bg-gray-100 text-gray-500"
+                }`}
+              >
                 Saved
               </button>
-              <button className="border text-sm lg:text-base px-2 py-1 lg:px-4 rounded hover:bg-gray-100 text-gray-500">
+              <button
+                onClick={() => setActiveIndex(1)}
+                className={`border text-sm lg:text-base px-2 py-1 lg:px-4 rounded  ${
+                  activeIndex === 1
+                    ? "border-black text-black font-bold"
+                    : "hover:bg-gray-100 text-gray-500"
+                }`}
+              >
                 Applied
               </button>
-              <button className="border text-sm lg:text-base px-2 py-1 lg:px-4 rounded hover:bg-gray-100 text-gray-500">
-                interviewing
+              <button
+                onClick={() => setActiveIndex(2)}
+                className={`border text-sm lg:text-base px-2 py-1 lg:px-4 rounded  ${
+                  activeIndex === 2
+                    ? "border-black text-black font-bold"
+                    : "hover:bg-gray-100 text-gray-500"
+                }`}
+              >
+                Interviewing
               </button>
-              <button className="border text-sm lg:text-base px-2 py-1 lg:px-4 rounded hover:bg-gray-100 text-gray-500">
+              <button
+                onClick={() => setActiveIndex(3)}
+                className={`border text-sm lg:text-base px-2 py-1 lg:px-4 rounded  ${
+                  activeIndex === 3
+                    ? "border-black text-black font-bold"
+                    : "hover:bg-gray-100 text-gray-500"
+                }`}
+              >
                 Rejected
               </button>
-              <button className="border text-sm lg:text-base px-2 py-1 lg:px-4 rounded hover:bg-gray-100 text-gray-500">
+              <button
+                onClick={() => setActiveIndex(4)}
+                className={`border text-sm lg:text-base px-2 py-1 lg:px-4 rounded  ${
+                  activeIndex === 4
+                    ? "border-black text-black font-bold"
+                    : "hover:bg-gray-100 text-gray-500"
+                }`}
+              >
                 Hidden
               </button>
             </div>
             <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 md:items-center md:space-x-4 mt-2 p-4">
               <span className="text-2xl font-bold">
-                Your saved jobs ({savedJobs.length})
+                {activeIndex === 0
+                  ? `Your saved jobs (${savedJobs.length})`
+                  : activeIndex === 1
+                  ? `Your applied jobs (${appliedJobs.length})`
+                  : activeIndex === 2
+                  ? `Your interviewing jobs (${interviewingJobs.length})`
+                  : activeIndex === 3
+                  ? `Your rejected jobs (${rejectedJobs.length})`
+                  : `Your hidden jobs (${hiddenJobs.length})`}
               </span>
               <div>
                 <button className="underline font-medium">
@@ -131,25 +317,7 @@ const Page = () => {
               </div>
             </div>
           </div>
-          <div className="infinite-scroll-component__outerdiv">
-            <div className="infinite-scroll-component ">
-              <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-                {renderJDcard()}
-              </div>
-            </div>
-          </div>
-          {savedJobs ? null : (
-            <div className="flex flex-col flex-auto items-center p-8 my-8 border rounded-xl m-4">
-              <span className="text-xl font-bold">No saved jobs</span>
-              <span className="mt-2 font-light">
-                Start &nbsp;
-                <Link className="text-blue-600" href="/">
-                  adding jobs
-                </Link>
-                &nbsp; to your list.
-              </span>
-            </div>
-          )}
+          {renderContent()}
         </div>
       </div>
     </div>
