af0ca155fa857d94b80ea4f308a14b04f8e1c165
MichaelSukkiSong <sa77irak@gmail.com>
2024-02-01 17:38:51 -0800

    added suspense to location bar

--- src/app/(main)/components/panel/main/Optionsbar.js
+++ src/app/(main)/components/panel/main/Optionsbar.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { Suspense } from "react";
 import LocationBtn from "./optionsbar/LocationBtn";
 import RemoteOnlyBtn from "./optionsbar/RemoteOnlyBtn";
 import RoleBtn from "./optionsbar/RoleBtn";
@@ -11,7 +14,9 @@ const Optionsbar = ({ setFilteringOptions }) => {
       <div className="flex justify-center">
         <div className="flex flex-col w-full flex-auto xl:flex-none xl:flex-row xl:items-center space-y-2 xl:space-y-0 xl:space-x-2 text-sm font-medium">
           <div className="flex flex-row items-center space-x-2 grow">
-            <LocationBtn setFilteringOptions={setFilteringOptions} />
+            <Suspense fallback={<p>Loading Location bar...</p>}>
+              <LocationBtn setFilteringOptions={setFilteringOptions} />
+            </Suspense>
             <RemoteOnlyBtn setFilteringOptions={setFilteringOptions} />
           </div>
           <div className="flex flex-row items-center space-x-2 grow shrink-0">
