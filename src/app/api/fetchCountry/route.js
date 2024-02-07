6cdb36f865eff54dbaa5b8a39161d6d6ebb77d01
MichaelSukkiSong <sa77irak@gmail.com>
2024-02-07 14:55:55 -0800

    revalidate country data every hour in cache

--- src/app/api/fetchCountry/route.js
+++ src/app/api/fetchCountry/route.js
@@ -1,5 +1,6 @@
 export async function GET() {
   const res = await fetch("https://restcountries.com/v3.1/all?fields=name", {
+    next: { revalidate: 3600 },
     headers: {
       "Content-Type": "application/json",
       // "API-Key": process.env.DATA_API_KEY,
