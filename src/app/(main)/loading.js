ce5ea2ad974af7b21dd3f041dfaf0b1a45a40222
MichaelSukkiSong <sa77irak@gmail.com>
2024-02-27 15:00:33 -0800

    moved loading to center

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col justify-start items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
       <iframe
         src="https://lottie.host/embed/ffcbf3fd-52ff-4bf0-a4f4-9218f3fa44f3/eT8ajCvyLR.json"
         type="text/html"
